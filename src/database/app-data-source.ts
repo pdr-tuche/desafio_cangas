ea099b09a5e426b4c4cb4ac2b0c67f998272a529
Pedro <contato.nevespedro@gmail.com>
2023-11-02 08:10:31 -0300

    importado dotenv

--- src/database/app-data-source.ts
+++ src/database/app-data-source.ts
@@ -1,15 +1,15 @@
+import "dotenv/config";
 import { DataSource } from "typeorm";
 
 export const PostgresDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: ["src/entities/*.ts"],
     migrations: ["src/database/migrations/*ts"],
-    migrationsTableName: "_prisma_migrations",
 });
 
 PostgresDataSource.initialize()
